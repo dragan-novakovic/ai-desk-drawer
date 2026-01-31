ff98e04de4eda98f677121a5eef1f0d56b8a51a4
dragan1810 <d.novakovic1810@yahoo.com>
2026-01-31 13:00:21 +0100

    qq

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 import FreeCAD as App
+import FreeCADGui as Gui
 import Part
 
 doc = App.newDocument("PartTest")
 
 box = Part.makeBox(10, 20, 30)
-Part.show(box)
+obj = doc.addObject("Part::Feature", "Box")
+obj.Shape = box
+
+# Set the box color to yellow
+Gui.ActiveDocument.getObject(obj.Name).ShapeColor = (1.0, 1.0, 0.0)
 
 doc.recompute()
+Gui.SendMsgToActiveView("ViewFit")
